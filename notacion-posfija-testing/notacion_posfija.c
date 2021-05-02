dd4dd2eb7aa8f6c7c77ff6c70228db2df80c6001
Kevin Mitnick <kvasquez@fi.uba.ar>
2021-05-02 20:46:46 -0300

    feat(notacion-posfija-testing)

---
+++ notacion-posfija-testing/notacion_posfija.c
@@ -0,0 +1,123 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+#include "notacion_posfija.h"
+#include "pila.h"
+#include "cola.h"
+
+#define SUMA "+"
+#define RESTA "-"
+#define DIVISION "/"
+#define OPERADOR_TERNARIO "?"
+
+int sumar(int *numero_a, int *numero_b) {
+  return *numero_b + *numero_a;
+}
+
+int restar(int *numero_a, int *numero_b) {
+  return *numero_b - *numero_a;
+}
+
+int dividir(int *numero_a, int *numero_b) {
+  return *numero_b / *numero_a;
+}
+
+int terciarizar(int *numero_a, int *numero_b, int *numero_c) {
+  return *numero_c ? *numero_b : *numero_a;
+}
+
+bool es_operador(char *str) {
+  return !strcmp(str, SUMA) || !strcmp(str, RESTA) || !strcmp(str, DIVISION) || !strcmp(str, OPERADOR_TERNARIO);
+}
+
+bool operador_es_binario(char *str) {
+  return !strcmp(str, SUMA) || !strcmp(str, RESTA) || !strcmp(str, DIVISION);
+}
+
+bool operador_es_ternario(char *str) {
+  return !strcmp(str, OPERADOR_TERNARIO);
+}
+
+bool validar_notacion_posfija(char **strv) {
+  int count = 0;
+  for(size_t i = 0; strv[i]; i++) {
+    if(!es_operador(strv[i])) {
+      count++;
+    } else {
+      if(operador_es_binario(strv[i])) {
+        if(count < 2) {
+          return false;
+        } else {
+          count--;
+        }
+      } else if(operador_es_ternario(strv[i])) {
+        if(count < 3) {
+          return false;
+        } else {
+          count -= 2;
+        }
+      }
+    } 
+  }
+  return true;
+}
+
+void operador_binario(pila_t *pila_numeros, int operacion(int *, int *)) {
+  int *a = (int *)pila_desapilar(pila_numeros);
+  int *b = (int *)pila_desapilar(pila_numeros);
+  int *resultado = calloc(1, sizeof(int));
+  *resultado = operacion(a, b);
+  free(a);
+  free(b);
+  pila_apilar(pila_numeros, resultado);
+}
+
+void operador_ternario(pila_t *pila_numeros, int operacion(int *, int *, int *)) {
+  int *a = (int *)pila_desapilar(pila_numeros);
+  int *b = (int *)pila_desapilar(pila_numeros);
+  int *c = (int *)pila_desapilar(pila_numeros);
+  int *resultado = calloc(1, sizeof(int));
+  *resultado = operacion(a, b, c);
+  free(a);
+  free(b);
+  free(c);
+  pila_apilar(pila_numeros, resultado);
+}
+
+void operar(char **strv, pila_t *pila_numeros) {
+  for(size_t i = 0; strv[i]; i++) {
+    if(!es_operador(strv[i])) {
+      int *numero = calloc(1, sizeof(int));
+      *numero = atoi(strv[i]);
+      pila_apilar(pila_numeros, numero);
+    } else {
+      if(!strcmp(strv[i], SUMA)) {
+        operador_binario(pila_numeros, sumar);
+      }
+      if(!strcmp(strv[i], RESTA)) {
+        operador_binario(pila_numeros, restar);
+      }
+      if(!strcmp(strv[i], DIVISION)) {
+        operador_binario(pila_numeros, dividir);
+      }
+      if(!strcmp(strv[i], OPERADOR_TERNARIO)) {
+        operador_ternario(pila_numeros, terciarizar);
+      }
+    }
+  }
+}
+
+int notacion_posfija(char **strv) {
+  pila_t *pila_numeros = pila_crear();
+  operar(strv, pila_numeros);
+  int resultado = *(int *) pila_ver_tope(pila_numeros);
+  while(!pila_esta_vacia(pila_numeros)) {
+    free(pila_desapilar(pila_numeros));
+  }
+  pila_destruir(pila_numeros);
+  return resultado;
+}
+
---
+++ notacion-posfija-testing/pruebas_notacion_posfija.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include "notacion_posfija.h"
+#include "testing.h"
+#include "strutil.h"
+
+void pruebas_operaciones_notacion_posfija() {
+   printf("--------------------------------------------------\n");
+   printf("PRUEBAS OPERACIONES NOTACIÓN POSFIJA\n");
+   printf("--------------------------------------------------\n");
+
+   char *str_1 = "5 3 +";
+   char *str_2 = "5 3 -";
+   char *str_3 = "5 3 /";
+   char *str_4 = "3 5 8 + +";
+   char *str_5 = "3 5 8 + -";
+   char *str_6 = "3 5 - 8 +";
+   char *str_7 = "1 -1 0 ?";
+
+   char **strv_1 = split(str_1, ' ');
+   char **strv_2 = split(str_2, ' ');
+   char **strv_3 = split(str_3, ' ');
+   char **strv_4 = split(str_4, ' ');
+   char **strv_5 = split(str_5, ' ');
+   char **strv_6 = split(str_6, ' ');
+   char **strv_7 = split(str_7, ' ');
+
+   print_test("5 3 + --> 8", 8 == notacion_posfija(strv_1));
+   print_test("5 3 - --> 2", 2 == notacion_posfija(strv_2));
+   print_test("5 3 / --> 1", 1 == notacion_posfija(strv_3));
+   print_test("3 5 8 + + --> 16", 16 == notacion_posfija(strv_4));
+   print_test("3 5 8 + - --> -10", -10 == notacion_posfija(strv_5));
+   print_test("3 5 - 8 + --> 6", 6 == notacion_posfija(strv_6));
+   print_test("1 -1 0 ? --> -1", -1 == notacion_posfija(strv_7));
+
+   free_strv(strv_1);
+   free_strv(strv_2);
+   free_strv(strv_3);
+   free_strv(strv_4);
+   free_strv(strv_5);
+   free_strv(strv_6);
+   free_strv(strv_7);
+}
+
+void pruebas_formato_valido() {
+   printf("--------------------------------------------------\n");
+   printf("PRUEBAS FORMATO VALIDO\n");
+   printf("--------------------------------------------------\n");
+
+   char *str_1 = "5 3 +";
+   char *str_2 = "5 3 -";
+   char *str_3 = "5 3 /";
+   char *str_4 = "3 5 8 + +";
+   char *str_5 = "3 5 8 + -";
+   char *str_6 = "3 5 - 8 +";
+   char *str_7 = "1 -1 0 ?";
+   char *str_8 = "2 2 + +";
+   char *str_9 = "0 1 ?";
+
+   char **strv_1 = split(str_1, ' ');
+   char **strv_2 = split(str_2, ' ');
+   char **strv_3 = split(str_3, ' ');
+   char **strv_4 = split(str_4, ' ');
+   char **strv_5 = split(str_5, ' ');
+   char **strv_6 = split(str_6, ' ');
+   char **strv_7 = split(str_7, ' ');
+   char **strv_8 = split(str_8, ' ');
+   char **strv_9 = split(str_9, ' ');
+
+   print_test("5 3 + es correcto", validar_notacion_posfija(strv_1));
+   print_test("5 3 - es correcto", validar_notacion_posfija(strv_2));
+   print_test("5 3 / es correcto", validar_notacion_posfija(strv_3));
+   print_test("3 5 8 + + es correcto", validar_notacion_posfija(strv_4));
+   print_test("3 5 8 + - es correcto", validar_notacion_posfija(strv_5));
+   print_test("3 5 - 8 + es correcto", validar_notacion_posfija(strv_6));
+   print_test("1 -1 0 ? es correcto", validar_notacion_posfija(strv_7));
+   print_test("2 2 + + no es correcto", !validar_notacion_posfija(strv_8));
+   print_test("0 1 ? no es correcto", !validar_notacion_posfija(strv_9));
+
+   free_strv(strv_1);
+   free_strv(strv_2);
+   free_strv(strv_3);
+   free_strv(strv_4);
+   free_strv(strv_5);
+   free_strv(strv_6);
+   free_strv(strv_7);
+   free_strv(strv_8);
+   free_strv(strv_9);
+}
+
+void pruebas_notacion_posfija() {
+  pruebas_operaciones_notacion_posfija();
+  pruebas_formato_valido();
+}
